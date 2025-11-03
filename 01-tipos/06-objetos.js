308c0680764542c401847f108f3ccfba706ae9f7
Mateo1424 <maescobar@frsn.utn.edu.ar>
2025-11-03 01:09:02 -0300

    nueva carpeta

---
+++ 01-tipos/06-objetos.js
@@ -0,0 +1,13 @@
+let nombre = "mateo";
+let edad = 19;
+let apellido = "pomponio";
+
+let personaje = {
+    nombre: "matute",
+    apellido: "pompi",
+    edad: 20,
+};
+
+console.log(personaje);
+console.log(personaje.nombre);
+console.log(personaje['apellido']);
